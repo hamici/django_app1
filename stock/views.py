42af4bde3a7e92764c2143659a9db181414cc00d
Mohamed Hamici <moh.hamici@hotmail.fr>
2023-04-25 15:05:12 +0200

    correction2 Exercice

--- stock/views.py
+++ stock/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homePage(request):
-    return render(request,'listing/home.html')
+    return render(request,'stock/home.html')
